d887c18c2fe451ceff6785a75cbd21aa6b6c2bbc
David-Saini <lakshaysaini92558@gmail.com>
2024-02-02 21:59:24 +0530

    footer

---
+++ components/FaqSec.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FaqSec = () => {
+    return (
+        <div>FaqSec</div>
+    )
+}
+
+export default FaqSec
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import Footer from "@/components/Footer";
 import NavBar from "@/components/common/Navbar";
 
 export default function Home() {
   return (
     <>
       <div className="bg-white">
-        <NavBar />
+        {/* <NavBar /> */}
+        <Footer />
       </div>
     </>
   );
